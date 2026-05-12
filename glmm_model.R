d2cd0a2eb9047f5100ae78975c01c2838ee78675
zhaokangshun888 <zhaokangshun@ihb.ac.cn>
2026-05-12 16:13:24 +0300

    Update glmm_model.R

--- glmm_model.R
+++ glmm_model.R
@@ -1,4 +1,3 @@
-
 ###############################################################################################       
 ############################################################################################### Model-Similarity 
 ############################################################################################### 
@@ -10,93 +9,100 @@ library(nlme)
 library(DHARMa)
 library(car)
 
-#######Load data 
-data <- read.csv("Similarity_with_predictors_model_1720.csv")
-######Remove missing values in explanatory variables 
+# Load data 
+data <- read.csv("Similarity_with_predictors_model_1720-2.csv")
+# Remove missing values in explanatory variables 
 data <- na.omit(data)
 head(data)
 hist(data$Similarity_Centroid)
-
-######Standardize fixed effect variables (z-score) 
-predictors <- c("MAT", "TS", "DIS", "RAD", "GDP", "NSR", "RBA", "CSI")
+# Standardize fixed effect variables (z-score) 
+predictors <- c("MAT", "TS", "PRD","SRD","CSI", "NSR", "RBA", "GDP")
 data[predictors] <- scale(data[predictors])
 
-###### VIF check
-#models examining the effects of environmental and anthropogenic factors on similarity between native and non-native freshwater fishes at basin scale
-vif_Model1 <- lm(Similarity_Centroid ~ MAT + TS + DIS + RAD + NSR + RBA + GDP + CSI, data = data)
-vif(vif_Model1)
+# vif check
+lm_check1 <- lm(Similarity_Centroid ~ MAT+TS+PRD+SRD+CSI+NSR+RBA+GDP, data = data)
+vif(lm_check1)
 
-vif_Model2 <- lm(Similarity_Centroid ~ MAT + TS + DIS + RAD + NSR + RBA + GDP + CSI + 
-                   MAT*TS + DIS*RAD + MAT*DIS + MAT*RAD + TS*RAD, data = data) #TS*DIS was removed due to its high VIF
-vif(vif_Model2)
+lm_check2 <- lm(Similarity_Centroid ~ MAT+TS+PRD+SRD+CSI+NSR+RBA+GDP+ MAT*TS+ PRD*SRD+ MAT*PRD+ MAT*SRD+ TS*PRD+ TS*SRD, data = data) 
+vif(lm_check2)
 
-vif_Model3 <- lm(Similarity_Centroid  ~ MAT + TS + DIS + RAD + NSR + RBA + GDP + CSI + 
-                   DIS*RAD + MAT*DIS + MAT*RAD + TS*RAD, data = data)  #TS*DIS was removed due to its high VIF
-vif(vif_Model3)
+lm_check3 <- lm(Similarity_Centroid ~ MAT+TS+PRD+SRD+CSI+NSR+RBA+GDP +MAT*PRD+ MAT*SRD+ TS*PRD+ TS*SRD, data = data)
+vif(lm_check3)
 
-vif_Model4 <- lm(Similarity_Centroid  ~ MAT + TS + DIS + RAD + NSR + RBA + GDP + CSI + 
-                   MAT*TS + MAT*DIS + MAT*RAD + TS*RAD, data = data) #TS*DIS was removed due to its high VIF
-vif(vif_Model4)
+lm_check4 <- lm(Similarity_Centroid ~ MAT+TS+PRD+SRD+CSI+NSR+RBA+GDP+ MAT*TS+ MAT*PRD+ MAT*SRD+ TS*PRD+ TS*SRD, data = data)
+vif(lm_check4)
 
-#model examining whether the main anthropogenic driver modifies ecological relationships between main environmental factors and the similarity between native and non-native freshwater fishes at the basin scale
-vif_Model <- lm(Similarity_Centroid ~ MAT + TS + DIS + RAD + NSR + RBA + GDP + CSI + 
-                  MAT*TS + MAT*DIS+ MAT*RAD + TS*RAD + MAT*GDP + TS*GDP, data = data)#TS*DIS was removed due to its high VIF
-vif(vif_Model)
+lm_check5 <- lm(Similarity_Centroid ~ MAT+TS+PRD+SRD+CSI+NSR+RBA+GDP+ PRD*SRD+ MAT*PRD+ MAT*SRD+ TS*PRD+ TS*SRD, data = data)
+vif(lm_check5)
 
 
+lm_check6 <- lm(Similarity_Centroid ~ MAT+TS+PRD+SRD+CSI+NSR+RBA+GDP+ TS*GDP + PRD*GDP+ TS*RBA + PRD*RBA, data = data)
+vif(lm_check6)
 
 ############################################################# glmm + spatial effect
 data$pos <- numFactor(scale(data$Med_Longit), scale(data$Med_Latit))
-data$group <- factor(rep(1, nrow(data)))  # Dummy group for global spatial structure 
-  
+data$group <- factor(rep(1, nrow(data)))
+
 #Model-1 only main factors
 glmm_s1 <- glmmTMB(
-  Similarity_Centroid  ~ MAT+TS + DIS+RAD + NSR + RBA + GDP + CSI  + (1|Ecoregion) + mat(pos + 0 | group), data = data, family = gaussian(link = "logit"))
-
+  Similarity_Centroid  ~ MAT+TS+PRD+SRD+CSI+NSR+RBA+GDP + mat(pos + 0 | group), data = data, family = gaussian(link = "logit"))
 summary(glmm_s1)
-sims1 <- simulateResiduals(glmm_s1)
-plot(sims1)
 
-#Model-4 remove DIS*RAD and retain MAT*TS
-glmm_s4 <- glmmTMB(
-  Similarity_Centroid  ~ MAT+TS + DIS+RAD + NSR + RBA + GDP + CSI + MAT*TS + MAT*DIS+ MAT*RAD + TS*RAD + (1|Ecoregion) + mat(pos + 0 | group), 
-  data = data, family = gaussian(link = "logit"))
+#Model-3-5 with interaction terms
+glmm_s2 <- glmmTMB(Similarity_Centroid  ~ MAT+TS+PRD+SRD+CSI+NSR+RBA+GDP+ MAT*TS+ PRD*SRD+ MAT*PRD+ MAT*SRD+ TS*PRD+ TS*SRD + mat(pos + 0 | group), 
+                   data = data, family = gaussian(link = "logit"))
+summary(glmm_s2)
 
+glmm_s3 <- glmmTMB(Similarity_Centroid  ~ MAT+TS+PRD+SRD+CSI+NSR+RBA+GDP +MAT*PRD+ MAT*SRD+ TS*PRD+ TS*SRD + mat(pos + 0 | group), 
+                   data = data, family = gaussian(link = "logit"))
+summary(glmm_s3)
+
+glmm_s4 <- glmmTMB(Similarity_Centroid  ~ MAT+TS+PRD+SRD+CSI+NSR+RBA+GDP+ MAT*TS+ MAT*PRD+ MAT*SRD+ TS*PRD+ TS*SRD + mat(pos + 0 | group), 
+                   data = data, family = gaussian(link = "logit"))
 summary(glmm_s4)
-sims4 <- simulateResiduals(glmm_s4)
-plot(sims4)
 
-#Model with interaction terms consider the impact of human activities-GDP 
-glmm_s <- glmmTMB(
-  Similarity_Centroid  ~ MAT+TS + DIS+RAD + NSR + RBA + GDP + CSI + MAT*TS + MAT*DIS+ MAT*RAD + TS*RAD + MAT*GDP + TS*GDP+ (1|Ecoregion) + mat(pos + 0 | group), 
-  data = data, family = gaussian(link = "logit"))
-
-summary(glmm_s)
-sims <- simulateResiduals(glmm_s)
-plot(sims)
+glmm_s5 <- glmmTMB(Similarity_Centroid  ~ MAT+TS+PRD+SRD+CSI+NSR+RBA+GDP+ PRD*SRD+ MAT*PRD+ MAT*SRD+ TS*PRD+ TS*SRD + mat(pos + 0 | group), 
+                   data = data, family = gaussian(link = "logit"))
+summary(glmm_s5)
 
 
+#Model-6 with interaction terms consider the impact of GDP and RBA
+glmm_s6 <- glmmTMB(Similarity_Centroid  ~ MAT+TS+PRD+SRD+CSI+NSR+RBA+GDP+ TS*GDP + PRD*GDP+ TS*RBA + PRD*RBA + mat(pos + 0 | group), 
+                   data = data, family = gaussian(link = "logit"))
+summary(glmm_s6)
 
-#######################################################check the spatial autocorrelation and R2
-######Moran's I
+
+#Check model residuals
+sims5 <- simulateResiduals(glmm_s5)
+plot(sims5)
+res_raw <- residuals(glmm_s5, type = "response")
+hist(res_raw,
+     breaks = 20,
+     col = "grey80",
+     border = "black",
+     main = "Histogram of model residuals",
+     xlab = "Residuals")
+
+# Calculate Moran's I
 library(spdep)
 coords <- cbind(data$Med_Longit, data$Med_Latit)
 nb <- knn2nb(knearneigh(coords, k=4))
 lw <- nb2listw(nb, style = "W", zero.policy = TRUE)
-resid_vals <- residuals(glmm_s4, type = "pearson")
+resid_vals <- residuals(glmm_s5, type = "pearson")
 moran.test(resid_vals, lw, zero.policy = TRUE)
 
-######R2
+# Calculate R²
 library(MuMIn)
-r2_values <- r.squaredGLMM(glmm_s4)
+r2_values <- r.squaredGLMM(glmm_s5)
 r2_values
 
-# Plot marginal effect
+
+# Plot
 library(graphics)
 library(sjPlot)
 library(ggplot2)
 
-p<-plot_model(glmm_s6, type = "pred", terms = c("MAT"),colors ="#FF9900")
+p<-plot_model(glmm_s5, type = "pred", terms = c("TS"),colors ="#FF9900")
 p+geom_line(size=1.5,colour="#FF9900")+theme_bw()+
   theme(panel.grid = element_blank())+
   theme(axis.text.x = element_text(size =16,colour="black"),
@@ -104,24 +110,21 @@ p+geom_line(size=1.5,colour="#FF9900")+theme_bw()+
         axis.title.x = element_text(size =16,colour="black"),
         axis.title.y = element_text(size =16,colour="black"))+
   theme()+
-  labs(x="MAT",y="Predicted values of similarity")+theme(axis.title = element_text(size = 12))+
+  labs(x="TS",y="Predicted values of similarity")+theme(axis.title = element_text(size = 12))+
   theme(legend.position = "top",plot.title = element_blank())
 
-ggsave("Marginal effect MAT and similarity.pdf", width = 5.5, height = 5)
+ggsave("Marginal effect TS and similarity.pdf", width = 5.5, height = 5)
 
- 
+
 # Plot
-p<-plot_model(glmm_s6, type = "pred", terms = c("TS"),colors =c("#146C36"))
+p<-plot_model(glmm_s5, type = "pred", terms = c("PRD"),colors =c("#146C36"))
 p+geom_line(size=1.5,colour="#146C36")+theme_bw()+
   theme(panel.grid = element_blank())+
   theme(axis.text.x = element_text(size =16,colour="black"),
         axis.text.y = element_text(size =16,colour="black"),
         axis.title.x = element_text(size =16,colour="black"),
         axis.title.y = element_text(size =16,colour="black"))+
-  labs(x="TS",y="Predicted values of Similarity")+theme(axis.title = element_text(size = 12))+
+  labs(x="PRD",y="Predicted values of Similarity")+theme(axis.title = element_text(size = 12))+
   theme(legend.position = "top",plot.title = element_blank())
 
-ggsave("Marginal effect TS and similarity.pdf", width = 5.5, height = 5)
-
-
-
+ggsave("Marginal effect PRD and similarity.pdf", width = 5.5, height = 5)
